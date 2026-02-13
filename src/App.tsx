d6151fc2ee4306f6465097debd1fe5bc07b8b855
Juan <juan@MacBook-Air-de-Juan.local>
2026-02-13 10:49:54 -0500

    commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import "./tailwind.css";
+import Home from "./pages/Home";
+// import './App.css'
+
+function App() {
+
+  return (
+    <div className="">
+      <Home/>
+    </div>
+  )
+}
+
+export default App
